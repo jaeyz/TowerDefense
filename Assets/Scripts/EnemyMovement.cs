c2e31952413c869a34897d173d75f416b3068f1b
jaeyz <njaeyz@gmail.com>
2013-05-20 17:28:48 +0800

    flooring texture

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMovement : MonoBehaviour {
+	
+	
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
