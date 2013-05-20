a59eb3ce2ac439315a28055dacaa6ab031bb92a8
jaeyz <njaeyz@gmail.com>
2013-05-20 14:59:07 +0800

    Utility classes

---
+++ Assets/Scripts/Utilities/AudioManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoSingleton<AudioManager> {
+	
+	public AudioClip mainMenuClip;
+	public AudioClip gameSceneClip;
+	public AudioClip gameOverClip;
+	
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	
+}
